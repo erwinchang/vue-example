ff7956bf5ef6acf3091dd876d9553f21e02f06b4
erwin <m927216@mail.ntust.edu.tw>
2021-09-28 02:08:02 -0700

    add menu icon button

--- src/App.vue
+++ src/App.vue
@@ -3,6 +3,18 @@
     <div id="nav">
       <nav class="navbar navbar-expand-lg navbar-dark bg-dark">
         <router-link to="/" class="navbar-brand">The Identity Store</router-link>
+
+        <button
+            class="navbar-toggler"
+            type="button"
+            data-toggle="collapse"
+            data-target="#navbarText"
+            aria-controls="navbarText"
+            aria-expanded="false"
+            aria-label="Toggle navigation">
+            <span class="navbar-toggler-icon" />
+        </button>
+
       </nav>
     </div>
     <router-view/>
