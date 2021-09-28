a4374ef047951c89ca06df0771f6723fbdfc437e
erwin <m927216@mail.ntust.edu.tw>
2021-09-28 02:04:52 -0700

    patch navbar

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,18 +1,22 @@
 <template>
-  <div class="home">
-    <img alt="Vue logo" src="../assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+  <div class="home container">
+    <Product />
+    <Cart />
   </div>
 </template>
 
 <script>
 // @ is an alias to /src
-import HelloWorld from '@/components/HelloWorld.vue'
+import Product from "../components/Product.vue"
+import Cart from "../components/Cart.vue"
+
+import { ref } from "vue"
 
 export default {
   name: 'Home',
   components: {
-    HelloWorld
+    Product,
+    Cart
   }
 }
 </script>
