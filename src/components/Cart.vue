42a94071fd3ededffdc160f05868f67265e3ee85
erwin <m927216@mail.ntust.edu.tw>
2021-09-28 20:30:10 -0700

    Cart.vue: add bootstrap list-group

--- src/components/Cart.vue
+++ src/components/Cart.vue
@@ -1,5 +1,36 @@
 <template>
-  <div class="cart">
-    <h1>This is an cart page</h1>
+  <div class="card">
+    <div class="card-body">
+      <h5 class="card-title">Your Cart</h5>
+    </div>
+
+    <ul class="list-group list-group-flush">
+      <li class="list-group-item d-flex justify-content-between align-items-center">
+        An item
+        <span class="badge badge-primary badge-pill">1</span>
+      </li>
+
+      <li class="list-group-item d-flex justify-content-between align-items-center">
+        An second item
+        <span class="badge badge-primary badge-pill">1</span>
+      </li>
+
+      <li class="list-group-item d-flex justify-content-between align-items-center">
+        An third item
+        <span class="badge badge-primary badge-pill">1</span>
+      </li>      
+    </ul>
+
+    <div class="card-body">
+       <router-link to="/shop" class="btn btn-primary btn-block">
+       Checkout
+       </router-link>
+    </div>
+
   </div>
 </template>
+
+<script>
+export default { 
+}
+</script>
