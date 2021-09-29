d16c1df4275f20b2644839a05f6cef6898eefa3e
erwin <m927216@mail.ntust.edu.tw>
2021-09-29 01:48:59 -0700

    fix pt-5

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,8 +1,9 @@
 <template>
   <div class="home container">
     <div class="row">
-      <div class="col-md-9 pot-5">
-        <div class="row row-cols-1 row-cols-sm-2 row-cols-md-3 row-cols-lg-3 row-cols-xl-3">
+      <div class="col-md-9 pt-5">
+
+       <div class="row row-cols-1 row-cols-sm-2 row-cols-md-3 row-cols-lg-3 row-cols-xl-3">
           <Product v-for="product in products" :product="product" :key="product.id" />
        </div>
       </div>
