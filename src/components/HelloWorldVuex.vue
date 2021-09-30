c91298bccba52ece3c335005a01a65b07b8f8ca7
erwin <m927216@mail.ntust.edu.tw>
2021-09-30 02:09:36 -0700

    patch vuex example

--- src/components/HelloWorldVuex.vue
+++ src/components/HelloWorldVuex.vue
@@ -1,5 +1,25 @@
 <template>
   <div class="cart">
-    <h1>This is an vuex ex01 page</h1>
+    <h1>This is an vuex name : [{{name}}] page</h1>
   </div>
 </template>
+
+<script>
+import { computed } from 'vue';
+import { useStore } from 'vuex'
+
+export default {
+  name: 'HelloWorldVuex',
+  setup () {
+    const store = useStore();
+
+    let name = computed(function () {
+      return store.state.name
+    })
+
+    return {
+      name
+    }
+  }
+}
+</script>
