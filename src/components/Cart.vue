e0fc5adf0632265978e02c6f08462f1c31a4aff8
erwin <m927216@mail.ntust.edu.tw>
2021-09-29 21:13:07 -0700

    patch Cart.vue/list item and totalPrice

--- src/components/Cart.vue
+++ src/components/Cart.vue
@@ -2,22 +2,19 @@
   <div class="card">
     <div class="card-body">
       <h5 class="card-title">Your Cart</h5>
+      <p v-if="cart.length == 0">
+        Your Cart is Empty
+      </p>
     </div>
 
     <ul class="list-group list-group-flush">
-      <li class="list-group-item d-flex justify-content-between align-items-center">
-        An item
-        <span class="badge badge-primary badge-pill">1</span>
+      <li v-for="item in cart" :key="item.id" class="list-group-item d-flex justify-content-between align-items-center">
+        {{item.name}}
+        <span class="badge badge-primary badge-pill">{{item.quantity}}</span>
       </li>
 
       <li class="list-group-item d-flex justify-content-between align-items-center">
-        An second item
-        <span class="badge badge-primary badge-pill">1</span>
-      </li>
-
-      <li class="list-group-item d-flex justify-content-between align-items-center">
-        An third item
-        <span class="badge badge-primary badge-pill">1</span>
+        Price <b>${{totalPrice}}</b>
       </li>      
     </ul>
 
@@ -31,6 +28,28 @@
 </template>
 
 <script>
+import {computed} from "vue";
+import {useStore} from "vuex";
+
 export default { 
+  name: "Cart",
+  setup(){
+    const store = useStore();
+
+    let cart = computed(function (){
+      return store.state.cart
+    });
+
+    let totalPrice = computed(function(){
+      return cart.value.reduce((total, next) => {
+          return total + (next.quantity * next.price)
+      },0)
+    })
+
+    return {
+      cart,
+      totalPrice
+    }
+  }
 }
 </script>
