db9fd82f4ddb410e79d5053447fbb5f7e9a6cff0
erwin <m927216@mail.ntust.edu.tw>
2021-10-01 02:17:20 -0700

    add axios get /products

--- src/components/product/Product_List.vue
+++ src/components/product/Product_List.vue
@@ -2,19 +2,29 @@
   <div class="container is-fluid">
     <div class="tile is-ancestor">
       <div class="tile is-parent">
-        <ProductListItem :productItem="productItem" />
+        <ProductListItem />
       </div>
     </div>
   </div>
 </template>
 
 <script>
-import Product_List_Item from './Product_List_Item.vue'
+import { mapGetters } from 'vuex'
+import Product_List_Item from './Product_List_Item'
 
 export default {
   name: 'ProductList',
   components: {
     ProductListItem: Product_List_Item
+  },
+  computed: {
+    ...mapGetters([
+      'productItems'
+    ])
+  },
+  created () {
+    console.log('created')
+    this.$store.dispatch('getProductItems');
   }
 }
 </script>
