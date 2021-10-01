dcf85e41259e0fced42251544d9a2d5b3fdd7d80
erwin <m927216@mail.ntust.edu.tw>
2021-09-30 20:49:02 -0700

    patch example code

--- src/router/index.js
+++ src/router/index.js
@@ -1,19 +1,19 @@
 import { createRouter, createWebHistory } from 'vue-router'
-import Home from '../views/Home.vue'
+import CartList from '../components/cart/Cart_List.vue'
+import ProductList from '../components/product/Product_List.vue'
 
 const routes = [
   {
-    path: '/',
-    name: 'Home',
-    component: Home
+    path: '/inventory',
+    component: ProductList
+  },  
+  {
+    path: '/cart',
+    component: CartList
   },
   {
-    path: '/about',
-    name: 'About',
-    // route level code-splitting
-    // this generates a separate chunk (about.[hash].js) for this route
-    // which is lazy-loaded when the route is visited.
-    component: () => import(/* webpackChunkName: "about" */ '../views/About.vue')
+    path: '/',
+    redirect: '/inventory'
   }
 ]
 
--- src/store/index.js
+++ src/store/index.js
@@ -1,4 +1,6 @@
 import { createStore } from 'vuex'
+import product from './modules/product'
+import cart from './modules/cart'
 
 export default createStore({
   state: {
@@ -8,5 +10,7 @@ export default createStore({
   actions: {
   },
   modules: {
+    product,
+    cart
   }
 })
