bd1a958fc3e60915d67db7e881321cea4250a62b
erwin <m927216@mail.ntust.edu.tw>
2021-09-30 23:30:42 -0700

    add get /product

--- server.js
+++ server.js
@@ -1,9 +1,19 @@
 const express = require('express')
+const fs = require('fs')
+const path = require('path')
 const app = express()
+const PRODUCT_DATA_FILE = path.join(__dirname,'server-product-data.json')
 const port = 3001
 
 app.get('/',(req,res) => {
 	res.send('Hello World')
+})
+
+app.get('/product', (req,res) => {
+	fs.readFile(PRODUCT_DATA_FILE, (err,data) =>{
+		res.setHeader('Cache-Control','no-cache')
+		res.json(JSON.parse(data))
+	})
 })
 
 app.listen(port, () =>{
