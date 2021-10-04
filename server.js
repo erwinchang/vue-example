a69dfa13987c2c054747199f284349e25e59332b
erwin <m927216@mail.ntust.edu.tw>
2021-10-03 20:49:54 -0700

    add server-cart-data.json

--- server.js
+++ server.js
@@ -1,8 +1,10 @@
 const express = require('express')
+const bodyParser = require('body-parser')
 const fs = require('fs')
 const path = require('path')
 const app = express()
 const PRODUCT_DATA_FILE = path.join(__dirname,'server-product-data.json')
+const CART_DATA_FILE = path.join(__dirname, 'server-cart-data.json')
 const port = 3001
 
 app.get('/',(req,res) => {
@@ -10,10 +12,19 @@ app.get('/',(req,res) => {
 })
 
 app.get('/products', (req,res) => {
+	console.log("app.get /products")
 	fs.readFile(PRODUCT_DATA_FILE, (err,data) =>{
 		res.setHeader('Cache-Control','no-cache')
 		res.json(JSON.parse(data))
 	})
+})
+
+app.get('/cart', (req, res) => {
+  console.log("app.get /cart")
+  fs.readFile(CART_DATA_FILE, (err, data) => {
+    res.setHeader('Cache-Control', 'no-cache');
+    res.json(JSON.parse(data));
+  });
 })
 
 app.listen(port, () =>{
