5ea0669c65f7f3efd39d62353f6eb2374ef7946b
Erwin Chang <m9207216@mail.ntust.edu.tw>
2022-08-10 15:59:50 +0800

    patch example

--- my-taiwind-project/src/App.vue
+++ my-taiwind-project/src/App.vue
@@ -5,7 +5,7 @@ import HelloWorld from './components/HelloWorld.vue'
 </script>
 
 <template>
-  <HelloWorld msg="Vite + Vue" />
+  <h1 class="text-3xl font-bold underline"> Hello world!</h1>
 </template>
 
 <style scoped>
