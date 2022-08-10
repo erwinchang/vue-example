e03116cd107e4b2e8ed8cdbbad17059e07a10392
Erwin Chang <m9207216@mail.ntust.edu.tw>
2022-08-10 16:07:23 +0800

    patch button

--- my-taiwind-project/src/App.vue
+++ my-taiwind-project/src/App.vue
@@ -1,23 +1,32 @@
 <script setup>
-// This starter template is using Vue 3 <script setup> SFCs
-// Check out https://vuejs.org/api/sfc-script-setup.html#script-setup
-import HelloWorld from './components/HelloWorld.vue'
+import { Disclosure } from '@headlessui/vue'
+
+const transition = () => {
+ //
+}
 </script>
 
 <template>
-  <h1 class="text-3xl font-bold underline"> Hello world!</h1>
+  <div class="min-h-full">
+    <Disclosure as="nav" class="bg-gray-800">
+      <div class="max-w-7xl mx-auto px-4 sm:px-6 lg:px-8">
+        <div class="flex items-center justify-between h-16">
+          <div class="flex items-center">
+            <div class="hidden md:block">
+              <div class="ml-10 flex items-baseline space-x-4">
+                <button type="button" @click="transition" class="text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:ring-blue-300 font-medium rounded-lg text-sm px-5 py-2.5 dark:bg-blue-600 dark:hover:bg-blue-700 focus:outline-none dark:focus:ring-blue-800">Click to transition</button>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+    </Disclosure>
+    <main>
+      <div class="max-w-7xl mx-auto py-6 sm:px-6 lg:px-8">
+        <div class="px-4 py-6 sm:px-0">
+          <!-- ChildComponent goes here -->
+        </div>
+      </div>
+    </main>
+  </div>
 </template>
-
-<style scoped>
-.logo {
-  height: 6em;
-  padding: 1.5em;
-  will-change: filter;
-}
-.logo:hover {
-  filter: drop-shadow(0 0 2em #646cffaa);
-}
-.logo.vue:hover {
-  filter: drop-shadow(0 0 2em #42b883aa);
-}
-</style>
