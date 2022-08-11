dd023f16572155924bf35e0cb0dd470c85e09eb5
Erwin Chang <m9207216@mail.ntust.edu.tw>
2022-08-11 17:16:14 +0800

    patch example

--- my-vite/src/App.vue
+++ my-vite/src/App.vue
@@ -2,13 +2,18 @@
 export default{
   data(){
     return {
-      isChecked: false
+      selected: ''
     }
   }
 }
 </script>
 
 <template>
-  <input type="checkbox" id="checkbox"  v-model="isChecked" />
-  <label for="checkbox">Status: {{ isChecked }}</label>
+  <select v-model="selected">
+    <option disabled value="">請選擇</option>
+    <option value="台北市">台北市</option>
+    <option value="台中市">台中市</option>
+    <option value="台南市">台南市</option>
+  </select>
+  <p>Selected: {{ selected || '未選擇' }} </p>
 </template>
