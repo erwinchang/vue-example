7cdc6992d9cd52278ec73e6cc7cb1a4543116dec
Erwin Chang <m9207216@mail.ntust.edu.tw>
2022-08-11 15:01:15 +0800

    patch example

--- README.md
+++ README.md
@@ -1,5 +1,14 @@
 # vue-example
 
+採用vue 3.0.4, vite 1.0.0
 ```
 npm init vite-app my-vite
 ```
+
+[选项式 API (Options API)][1]
+
+- 使用选项式 API，我们可以用包含多个选项的对象来描述组件的逻辑
+- 例如 data、methods 和 mounted
+- 选项所定义的属性都会暴露在函数内部的 this 上，它会指向当前的组件实例
+
+[1]:https://cn.vuejs.org/guide/introduction.html#single-file-components
--- my-vite/src/App.vue
+++ my-vite/src/App.vue
@@ -1,15 +1,19 @@
-<template>
-  <img alt="Vue logo" src="./assets/logo.png" />
-  <HelloWorld msg="Hello Vue 3.0 + Vite" />
-</template>
-
 <script>
-import HelloWorld from './components/HelloWorld.vue'
-
-export default {
-  name: 'App',
-  components: {
-    HelloWorld
+export default{
+  data(){
+    return {
+      count: 0
+    }
   }
 }
 </script>
+
+<template>
+  <button @click='count++'>Count is: {{ count }}</button>
+</template>
+
+<style scoped>
+button {
+  font-weight: bold;
+}
+</style>
